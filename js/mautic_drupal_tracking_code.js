cc1c07629244f995244035be92fb1aee63a64c6c
Tommy Eliassen <pusle@pusle.com>
2016-08-15 16:42:31 +0200

    Get drupalSettings and pass to mautic function

--- js/mautic_drupal_tracking_code.js
+++ js/mautic_drupal_tracking_code.js
@@ -1,6 +1,12 @@
+(function () {
+    'use strict';
+    var settingsElement = document.querySelector('head > script[type="application/json"][data-drupal-selector="drupal-settings-json"], body > script[type="application/json"][data-drupal-selector="drupal-settings-json"]');
+    window.drupalSettings = (settingsElement !== null) ? JSON.parse(settingsElement.textContent) : {} ;
+})();
+url = drupalSettings.mautic_drupal.mautic_drupal_tracking_code.base_url + '/mtc.js';
 (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
     w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
         m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
-})(window,document,'script', + drupalSettings.mautic_drupal.mautic_drupal_tracking_code.base_url + '/mtc.js','mt');
+})(window,document,'script',url,'mt');
 
 mt('send', 'pageview');
